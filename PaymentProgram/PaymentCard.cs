4dee1d4abcfe83f60477578fe8a7d69d1db8d6a4
User <User@WIN-IQV1FL6VH10>
2023-04-07 17:09:13 +0300

    Добавьте файлы проекта.

---
+++ PaymentProgram/PaymentCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentProgram
+{
+    internal class PaymentCard
+    {
+        public string CardNumber;
+        public string CardExpDate;
+        public string CardholderName;
+        public int CardCVV;
+
+        public PaymentCard(string cardNumber, string cardExpDate, string cardholderName, int cardCVV)
+        { 
+        
+            CardNumber = cardNumber;
+            CardExpDate = cardExpDate;
+            CardholderName = cardholderName;
+            CardCVV = cardCVV;
+                    
+        }
+
+
+
+    }
+}
---
+++ PaymentProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PaymentProgram
+{
+    internal class Program
+    {   
+
+        static bool PrintCards(PaymentCard[] paymentCards) 
+        {
+        foreach (PaymentCard card in paymentCards) 
+            {
+            
+            Console.WriteLine("CardNumber: {0}, CardExpDate: {1}, CardholderName: {2}, Card CVV: {3}",
+                card.CardNumber, card.CardExpDate, card.CardholderName, card.CardCVV);
+            
+            }
+            return true;
+
+        }
+
+        static void Main(string[] args)
+         {
+            PaymentCard Card1 = new PaymentCard("1234 1235 1245 1254", "02/25", "Vasia Pupkin", 666);
+            PaymentCard Card2 = new PaymentCard("6666 1225 4569 1545", "05/24", "Levy Shtraus", 345);
+            PaymentCard Card3 = new PaymentCard("1518 1551 1515 4844", "06/27", "Andrew Warhola", 564);
+            PaymentCard Card4 = new PaymentCard("1515 4484 1544 3323", "04/25", "Pablo Picasso", 456);
+            PaymentCard Card5 = new PaymentCard("5155 8875 4848 4848", "08/25", "Vincent van Gogh", 358);
+
+            PaymentCard[] paymentCards = new PaymentCard[] { Card1, Card2, Card3, Card4, Card5 };
+
+            PrintCards(paymentCards);
+        }
+    }
+}
